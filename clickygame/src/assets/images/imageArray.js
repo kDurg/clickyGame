ad1e3d7233ae03bbca6c868ca6f9b5afb3924bed
kDurg <kyle.durigan@gmail.com>
2019-01-28 14:22:59 -0700

    updated psuedo coding

---
+++ clickygame/src/assets/images/imageArray.js
@@ -0,0 +1,14 @@
+// import Images from "../images";
+
+const imageArray = [
+    {name: "fox", image: "../images/img0.jpg", value: 0},
+    {name: "bison", image: "../images/img1.jpg", value: 1},
+    {name: "bunny", image: "../images/img2.jpg", value: 2},
+    {name: "giraffe", image: "../images/img3.jpg", value: 3},
+    {name: "pig", image: "../images/img4.jpeg", value: 4},
+    {name: "seal", image: "../images/img5.jpg", value: 5},
+    {name: "sloth", image: "../images/img6.jpg", value: 6},
+    {name: "unicorn", image: "../images/img7.jpg", value: 7},
+]
+
+export default imageArray;
--- clickygame/src/components/Cards.js
+++ clickygame/src/components/Cards.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import API from "../utils/API";
-
-import Images from "../../assets/images";
+import imageArray from "../assets/images/imageArray"
 
 class Populate extends React.Component {
     state = {
@@ -11,17 +9,54 @@ class Populate extends React.Component {
     }
 
     getAnimalPhotos () {
-        // for(i = 0; i<Images.length; i++)
-            console.log(Images.length);
+        for(var i = 0; i<imageArray.length; i++)
+            console.log(imageArray.length);
+            return this.name;
     }
 
-    componentDidMount() {
-        getAnimalPhotos();
+    componentDidMount(name) {
+        console.log(name)
+        this.getAnimalPhotos();
+        currentScore = 0;
+    }
+
+    handlePhotoClick() {
+        // needs to check selectedAnimal to selectedAnimalArray
+            // if the selectedAnimal.name is in selectedAnimalArray,
+                // end current game score, compare to top score and update if new high score
+                // reset current score to zero
+                // run imageRandomize function
+            // if selectedAnimal.name is NOT in selectedAnimalArray
+                // add name to selectedAnimalArray
+                // add 1 to score
+                // run imageRandomize function
+    }
+
+    imageRandomize () {
+        // loop through images and use a Math(function) to randomize placement in containing image boxes
+            // have to use a render? Need to change state?
+    }
+
+    compareChoice() {
+        if (selectedAnimal = selectedAnimalArray) {
+            console.log("Selection already part of array, you lose!");
+            compareScore();
+            imageRandomize()
+        }
+    }
+
+    compareScore() {
+        // take currentScore (const?) vs topScore (const?). 
+        if (currentScore >= topScore) {
+            topScore = currentScore
+        } else {
+            currentScore = 0;
+        }
     }
 
     render() {
         return (
-            <img className="card-img-top card" src=".../100px180/" alt="Card image cap" style="width: 18rem;"/>
+            <img className={this.name} src=".../100px180/" alt="Card image cap" style="width: 18rem;"/>
         )
     }
 
